dddcfadd4d29ef83d8c2bdf7f6a4886a72a3e6e4
unknown <provenchez@gmail.com>
2019-05-16 05:02:55 -0400

    hide modal on click

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,10 @@
 	
 	$("#theme")[0].play();
 
+	$("modal").bind("click", function(e) {
+		$("modal").hide();
+	});
+
 	$(".character").bind("click", function(e){
 		$(".border").removeClass("border");
 		$(e.target).addClass("border");
