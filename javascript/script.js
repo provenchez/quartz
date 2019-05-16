2ecf09ebf1a493b0226063aa339f4eaf53d2d7e1
unknown <provenchez@gmail.com>
2019-05-16 04:14:12 -0400

    sounds

--- javascript/script.js
+++ javascript/script.js
@@ -3,8 +3,9 @@
 	console.log('JQUERY LOADED', $)	
 	
 	$(".character").bind("click", function(e){
-		$(".border").removeClass("border")
+		$(".border").removeClass("border");
 		$(e.target).addClass("border");
+		$("#select")[0].play();
 	});
 
 	console.log($(".character"))
